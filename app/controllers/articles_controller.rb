90f04ee8ff3d2ac9e22816fea93fd92bdde285de
XessiveObserver <xessiveobserver@gmail.com>
2023-04-13 16:02:06 +0300

    fine tunning design and ux

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,14 +1,20 @@
 class ArticlesController < ApplicationController
+
   def index
     @articles = Article.includes(:publisher)
     @latest_articles = Article.order(created_at: :desc).limit(5)
     @random_articles = Article.order(Arel.sql('RANDOM()')).limit(5)
-    @news_travel_category = Article.where(category: "Travel").order(:category)      
-    @news_politics_category = Article.where(category: "Politics").order(:category)      
+    
+    @categories = Article.pluck(:category).uniq
+    @articles_by_category = {}
+    @categories.each do |category|
+      @articles_by_category[category] = Article.where(category: category)
+    end  
   end
 
   def show
     @article = Article.find(params[:id])
+    @related_articles = Article.where(category: @article.category).where.not(id: @article.id)
   end
 
   def new
