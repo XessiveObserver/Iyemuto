865e8c87606f08fb9aa336dcadffe7e653154510
XessiveObserver <xessiveobserver@gmail.com>
2023-04-19 15:36:13 +0300

    ux/ui final polishing

--- app/controllers/normal_pages_controller.rb
+++ app/controllers/normal_pages_controller.rb
@@ -1,5 +1,13 @@
 class NormalPagesController < ApplicationController
   def index
+    @articles = Article.includes(:publisher)
+    @latest_articles = Article.order(created_at: :desc).limit(5)
+    @random_articles = Article.order(Arel.sql('RANDOM()')).limit(5)
+    @categories = Article.pluck(:category).uniq
+    @articles_by_category = {}
+    @categories.each do |category|
+      @articles_by_category[category] = Article.where(category: category)
+    end 
   end
   def dashboard
   end
